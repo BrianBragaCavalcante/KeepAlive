f35ed52cc810ca6967aee7319577d31a0ca7a0d1
Brian Braga Cavalcante <brianbragacavalcantex@gmail.com>
2024-09-14 13:00:52 -0300

    Adding log

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
-from flask import Flask
+from flask import Flask, request
 import requests
 import time
 import json
 
 app = Flask(__name__)
 
-# Rename to this application link
-selfsite = 'https://keepalive-2hrf.onrender.com'
-
 with open('./sites.json', 'r') as file:
-	sites = dict(json.load(file))['sites']
+    sites = dict(json.load(file))['sites']
+
+def get_selfsite():
+    host = request.host_url.rstrip('/')
+    return host
 
 @app.route('/')
 def index():
-	result = {}
-	for site in sites:
-		result[site[8:]] = requests.get(site).status_code
-	return result
+    result = {}
+    for site in sites:
+        result[site[8:]] = requests.get(site).status_code
+    print(f'index: \n{result}')
+    return result
 
 @app.route('/turnon')
 def turn_on():
-	time.sleep(5 * 60)
-	requests.get(selfsite)
-	requests.get(selfsite+'/turnon')
+    time.sleep(5 * 60)
+    selfsite = get_selfsite()
+    requests.get(selfsite)
+    requests.get(selfsite+'/turnon')
+    print('turn_on: Call function')
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
