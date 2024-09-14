c879fbe314316a20094a3c9a5171dbf440b94a32
Brian Braga Cavalcante <brianbragacavalcantex@gmail.com>
2024-09-14 15:10:42 -0300

    Testing new method of linking websites

--- app.py
+++ app.py
@@ -8,25 +8,19 @@ app = Flask(__name__)
 with open('./sites.json', 'r') as file:
     sites = dict(json.load(file))['sites']
 
-def get_selfsite():
-    host = request.host_url.rstrip('/')
-    return host
-
 @app.route('/')
 def index():
     result = {}
     for site in sites:
         result[site[8:]] = requests.get(site).status_code
-    print(f'index: \n{result}')
+    print(f'index:\n{result}')
     return result
 
-@app.route('/turnon')
+@app.route('/turnon/')
 def turn_on():
-    time.sleep(5 * 60)
-    selfsite = get_selfsite()
-    requests.get(selfsite)
-    requests.get(selfsite+'/turnon')
-    print('turn_on: Call function')
+    while True:
+        time.sleep(5*60)
+        index()
 
 if __name__ == '__main__':
     app.run()
